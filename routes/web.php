8e6fc85f4953ef1d3cdfe6e592679f9249f394c2
Quentin Modena <quentinmodena@gmail.com>
2019-03-14 23:13:11 +0100

    First commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+  $router->get('users',  ['uses' => 'UserController@showAllUsers']);
+
+  $router->get('users/{id}', ['uses' => 'UserController@showOneUser']);
+
+  $router->post('users', ['uses' => 'UserController@create']);
+
+  $router->delete('users/{id}', ['uses' => 'UserController@delete']);
+
+  $router->put('users/{id}', ['uses' => 'UserController@update']);
+});
